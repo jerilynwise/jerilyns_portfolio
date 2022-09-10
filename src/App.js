f899832487f6bec9eff093a48857ffbe12167891
Jerilyn <jerilyn.wise@paddywax.com>
2022-09-10 13:27:34 -0500

    Project/Portfolio was done with the aide of a TA, contact form added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 import Portfolio from './components/Portfolio'
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <ContactForm></ContactForm>
         <About></About>
         <Portfolio></Portfolio>
       </main>
