5b8d2f5fd9d838ea6d54f2cd12e4e8de3f1cdd48
Jerilyn <jerilyn.wise@paddywax.com>
2022-09-10 17:39:09 -0500

    footer and footer styling added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
       <div>
         <Header></Header>
+        <Footer></Footer>
       </div>
   );
 }
